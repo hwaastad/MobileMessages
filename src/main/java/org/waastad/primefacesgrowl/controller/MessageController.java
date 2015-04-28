22fa552502445d2a6cda9f88f6ad0fd857ee7f46
Helge Waastad <helge.waastad@datametrix.no>
2015-04-28 19:51:39 +0200

    add global render mobile add two types of messages.

--- src/main/java/org/waastad/primefacesgrowl/controller/MessageController.java
+++ src/main/java/org/waastad/primefacesgrowl/controller/MessageController.java
@@ -7,9 +7,10 @@ package org.waastad.primefacesgrowl.controller;
 
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import lombok.extern.slf4j.Slf4j;
-import org.omnifaces.util.Messages;
 
 /**
  *
@@ -19,11 +20,17 @@ import org.omnifaces.util.Messages;
 @SessionScoped
 @Slf4j
 public class MessageController implements Serializable {
+
     private static final long serialVersionUID = 4460470331582972205L;
 
-    public String addMessage() {
+    public String addMessage() throws InterruptedException {
         log.info("Adding message");
-        Messages.addFlashGlobalInfo("A Flash message");
+//        Messages.createInfo("A normal Message");
+//        Messages.addFlashGlobalInfo("A Flash message");
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        context.addMessage(null, new FacesMessage("Successful", "Your message"));
+        context.addMessage(null, new FacesMessage("Second Message", "Additional Message Detail"));
         return "pm:second";
     }
 }
